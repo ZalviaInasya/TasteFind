8d931cf97c6c362785e3eeb030a1bf4a1d5f81c6
Nadia Mgh <nadiaamgdln@gmail.com>
2025-12-18 12:18:29 +0700

    fix gasi

--- frontend/src/pages/All.jsx
+++ frontend/src/pages/All.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import DetailModal from "../components/DetailModal";
 
 export default function All() {
   const [searchValue, setSearchValue] = useState("");
